e5cc0e42a329382d98b42630e13dd240ed6433d9
Vaishnaviraj5 <80109979+Vaishnaviraj5@users.noreply.github.com>
2022-05-04 14:26:54 +0530

    Add files via upload

---
+++ TernaryOperator.java
@@ -0,0 +1,10 @@
+public class TernaryOperator{
+	public static void main(String[] args){
+		int person1,person2;
+		person1=20;
+		person2=(person1==1)?61:90;
+		System.out.println("value of person2 is:"+person2);
+		person2=(person1==20)?61:90;
+		System.out.println("value of person2 is:"+person2);
+	}
+}
